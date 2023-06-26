ef8dfd182308b94fab248cf9b252a7559d4931a9
Lorena Garcia <mglorena@gmail.com>
2023-06-26 09:52:25 -0300

    Cambios

--- gethilo.py
+++ gethilo.py
@@ -5,7 +5,7 @@ consumer_key = "b1xEAoHh451I1qlOeiEC1p1ne"
 consumer_secret = "huB96moyJ6Dj8Jj2IfDwWdrIYS8ovdfIyoEdpCy65ajjC5Cv4Q"
 access_token = "15394306-38dsRVZ12evrNbYiimUsS1JXNoR9EtBEsvye6MxC2"
 access_token_secret = "r42oRgPgJekVBVQsJOvFKPN9vRd7NPNdSJjwXEYRDtd5p"
-#Modificando para poder tener permiso
+#Modificando para poder tener permiso, las claves fueron regeneradas
 
 # Autenticar con las credenciales de la API de Twitter
 auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
