aade054567e9c1591f819dd226369758480e49d8
Lorena Garcia <mglorena@gmail.com>
2023-06-26 10:31:00 -0300

    cambio

--- gethilo.py
+++ gethilo.py
@@ -1,10 +1,10 @@
 import tweepy
 
 # Configurar las credenciales de la API de Twitter
-consumer_key = "b1xEAoHh451I1qlOeiEC1p1ne"
-consumer_secret = "huB96moyJ6Dj8Jj2IfDwWdrIYS8ovdfIyoEdpCy65ajjC5Cv4Q"
-access_token = "15394306-38dsRVZ12evrNbYiimUsS1JXNoR9EtBEsvye6MxC2"
-access_token_secret = "r42oRgPgJekVBVQsJOvFKPN9vRd7NPNdSJjwXEYRDtd5p"
+consumer_key = ""
+consumer_secret = ""
+access_token = ""
+access_token_secret = ""
 #Modificando para poder tener permiso, las claves fueron regeneradas
 
 # Autenticar con las credenciales de la API de Twitter
@@ -23,4 +23,3 @@ comments = api.get_status(tweet_id, tweet_mode='extended').replies()
 # Imprimir los comentarios
 for comment in comments:
     print(comment.full_text)
-#fbSG8ltZno-u0rVXEjnNlsQAniHZx6R71U2uMh_Kp0eiQBvISO
